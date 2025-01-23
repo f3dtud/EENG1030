cf048e60db1c3410675338cad2ac6f6e2dcdb769
f3dtud <frank.duignan@tudublin.ie>
2025-01-23 15:25:33 +0000

    second commit

--- blinky/src/main.c
+++ blinky/src/main.c
@@ -18,7 +18,6 @@ void setup(void)
     RCC->AHB2ENR |= (1 << 1); // turn on GPIOB
     GPIOB->MODER |= (1 << 6); // Configure GPIOB, bit 3 as an output
     GPIOB->MODER &= ~(1 << 7);
-
 }
 void delay(volatile uint32_t dly)
 {
