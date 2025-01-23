63823dc2a6e31eb8db4187ac5dad0a73b4d5ae4b
f3dtud <frank.duignan@tudublin.ie>
2025-01-23 15:24:23 +0000

    second commit

--- blinky/src/main.c
+++ blinky/src/main.c
@@ -2,8 +2,6 @@
 #include <stdint.h>
 void setup(void);
 void delay(volatile uint32_t dly);
-void enablePullUp(GPIO_TypeDef *Port, uint32_t BitNumber);
-void pinMode(GPIO_TypeDef *Port, uint32_t BitNumber, uint32_t Mode);
 int main()
 {
     setup();
@@ -18,30 +16,11 @@ int main()
 void setup(void)
 {
     RCC->AHB2ENR |= (1 << 1); // turn on GPIOB
-  //  GPIOB->MODER |= (1 << 6); // Configure GPIOB, bit 3 as an output
-  //  GPIOB->MODER &= ~(1 << 7);
-    pinMode(GPIOB,3,1);
+    GPIOB->MODER |= (1 << 6); // Configure GPIOB, bit 3 as an output
+    GPIOB->MODER &= ~(1 << 7);
+
 }
 void delay(volatile uint32_t dly)
 {
     while(dly--);
-}
-void enablePullUp(GPIO_TypeDef *Port, uint32_t BitNumber)
-{
-	Port->PUPDR = Port->PUPDR &~(3u << BitNumber*2); // clear pull-up resistor bits
-	Port->PUPDR = Port->PUPDR | (1u << BitNumber*2); // set pull-up bit
-}
-void pinMode(GPIO_TypeDef *Port, uint32_t BitNumber, uint32_t Mode)
-{
-	/*
-        Modes : 00 = input
-                01 = output
-                10 = special function
-                11 = analog mode
-	*/
-	uint32_t mode_value = Port->MODER;
-	Mode = Mode << (2 * BitNumber);
-	mode_value = mode_value & ~(3u << (BitNumber * 2));
-	mode_value = mode_value | Mode;
-	Port->MODER = mode_value;
 }
