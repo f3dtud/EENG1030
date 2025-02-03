eeb1c30653efc3fc872038a963fd13c7d0c8263e
Frank Duignan <frank.duignan@gmail.com>
2025-02-03 18:38:55 +0000

    New example with the BMI160

---
+++ BMI160_L432/src/main.c
@@ -0,0 +1,41 @@
+#include "eeng1030_lib.h"
+void setup(void);
+void initI2C(void);
+void delay(volatile uint32_t dly);
+int main()
+{
+    setup();
+    while(1)
+    {
+        GPIOB->ODR |= (1 << 3);
+        delay(100000);        
+        GPIOB->ODR &= ~(1 << 3);
+        delay(100000);
+        I2C1->TXDR=0xaa;
+    }
+}
+void setup()
+{
+    RCC->AHB2ENR |= (1 << 0) | (1 << 1); // enable GPIOA and GPIOB
+    initI2C();    
+}
+void initI2C(void)
+{
+    initClocks();
+    pinMode(GPIOB,3,1); // make PB3 (internal LED) an output
+    pinMode(GPIOB,6,3); // alternative functions for PB6 and PB7
+    pinMode(GPIOB,7,3); 
+    selectAlternateFunction(GPIOB,6,4); // Alternative function 4 = I2C1_SCL
+    selectAlternateFunction(GPIOB,7,4); // Alternative function 4 = I2C1_SDA
+    RCC->APB1ENR1 |= (1 << 21); // enable I2C1
+    I2C1->TIMINGR = (3 << 28) + (4 << 20) + (2 << 16) + (0x0f << 8) + (0x13); // from table 138 in the reference manual
+    I2C1->ICR |= 0xffff; // clear all pending interrupts
+    I2C1->CR2 = 0x5a;
+    I2C1->CR1 |= (1 << 0);
+    
+
+}
+void delay(volatile uint32_t dly)
+{
+    while(dly--);
+}
